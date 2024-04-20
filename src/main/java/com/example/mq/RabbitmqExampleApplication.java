51de7c8efca399ff5cdbb14c5196d03de4d2be13
Resun <resunne@hotmail.com>
2024-04-20 22:42:15 +0800

    init.

---
+++ src/main/java/com/example/mq/RabbitmqExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqExampleApplication.class, args);
+    }
+
+}
